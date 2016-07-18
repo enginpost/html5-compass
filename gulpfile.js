0966ac0e2f935c8cad16664e605dcdb3f5fdbb42
Steve McDonald <smcdonald@Steves-MacBook-Pro.local>
2016-07-17 21:42:14 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+    
+    browserSync.init({
+        server: {
+            baseDir: "./site/"
+        }
+    });
+
+    gulp.watch('site/*.html').on('change', browserSync.reload);
+    gulp.watch('site/styles/*.css').on('change', browserSync.reload);
+    gulp.watch('site/scripts/*.js').on('change', browserSync.reload);
+    gulp.watch('site/images/*').on('change', browserSync.reload);
+});
+
+gulp.task('default',['serve']);
---
+++ site/scripts/main.js
@@ -0,0 +1,6 @@
+/* main js file*/
+(function($){
+  $(document).ready(function(){
+    alert('hello world!');
+  });
+})(jQuery);
